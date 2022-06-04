5c4b49d53a8eafd260465c94b3e49c50410786a9
Svetoslav Uzunov <53313278+SvetoslavUzunov@users.noreply.github.com>
2022-06-04 09:11:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace MergeSort;
+
+public class Program
+{
+    public static void Main()
+    {
+        var array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        Console.WriteLine(string.Join(" ", Splits(array)));
+    }
+
+    private static int[] Splits(int[] array)
+    {
+        if (array.Length == 1) return array;
+
+        int middle = array.Length / 2;
+        var leftPart = new int[middle];
+        var rightPart = new int[array.Length - middle];
+
+        for (int i = 0; i < middle; i++)
+        {
+            leftPart[i] = array[i];
+        }
+
+        for (int i = 0; i < array.Length - middle; i++)
+        {
+            rightPart[i] = array[(array.Length / 2) + i];
+        }
+
+        leftPart = Splits(leftPart);
+        rightPart = Splits(rightPart);
+        return Merge(leftPart, rightPart);
+    }
+
+    private static int[] Merge(int[] leftPart, int[] rightPart)
+    {
+        var leftIncrease = 0;
+        var rightIncrease = 0;
+        var mergeArray = new int[leftPart.Length + rightPart.Length];
+
+        for (int i = 0; i < mergeArray.Length; i++)
+        {
+            if (rightIncrease == rightPart.Length || ((leftIncrease < leftPart.Length) && (leftPart[leftIncrease] <= rightPart[rightIncrease])))
+            {
+                mergeArray[i] = leftPart[leftIncrease];
+                leftIncrease++;
+            }
+            else if (leftIncrease == leftPart.Length || ((rightIncrease < rightPart.Length) && (leftPart[leftIncrease] >= rightPart[rightIncrease])))
+            {
+                mergeArray[i] = rightPart[rightIncrease];
+                rightIncrease++;
+            }
+        }
+        return mergeArray;
+    }
+}
